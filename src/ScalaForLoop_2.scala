9c1cfedb6d3b77eb844352d79bc4e174c2b72219
scalakid <alaniit007@gmail.com>
2015-10-21 21:27:13 +1100

    Initial commit

---
+++ src/ScalaForLoop_2.scala
@@ -0,0 +1,14 @@
+/**
+ * Created by ADMIN on 21-Oct-15.
+ * Simple scala fo to print numbers from1 to 10, using 'until'
+ */
+object ScalaForLoop_2 {
+
+  def main(args:Array[String])
+  {
+    var i=0;
+
+    for(i<-1 until 11)
+      println(i)
+  }
+}
